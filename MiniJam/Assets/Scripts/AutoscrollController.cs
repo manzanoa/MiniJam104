007d3e780935dca4ca9061c3800a68e0a463f1f9
Minescratcher452 <minescratcher452@gmail.com>
2022-04-17 23:01:10 -0400

    foo

--- MiniJam/Assets/Scripts/AutoscrollController.cs
+++ MiniJam/Assets/Scripts/AutoscrollController.cs
@@ -8,7 +8,8 @@ public class AutoscrollController : MonoBehaviour
     public float CAMERA_SPEED = 1f; // Camera speed in UU per second
     public BoxCollider2D left, right, top, deathPlaneColl2D;
     public GameObject frog;
-    public BGMController BGMController;
+    public GameObject BGMController;
+    public GameObject AmbientController;
     private int moveCamera = 0;
 
     // Used to draw collision boxes in editor
@@ -18,25 +19,46 @@ public class AutoscrollController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(moveCamera == 0 && Input.GetKeyDown(KeyCode.Mouse0))
+        if (moveCamera == 0 && Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Debug.Log("Pressed");
-            ActivateCamera(); 
+            ActivateCamera();
         }
 
         // Move the camera up slowly
         transform.position = new Vector3(transform.position.x, transform.position.y + (CAMERA_SPEED * Time.deltaTime * moveCamera), transform.position.z);
 
-        if(frog.GetComponent<FrogMovement>().gameOver)
+        if (frog.GetComponent<FrogMovement>().gameOver)
         {
-            CAMERA_SPEED = 0;
+            BGMController.SetActive(false);
+            moveCamera = 0;
         }
     }
 
     public void ActivateCamera()
     {
         moveCamera = 1;
-        BGMController.StartLoop();
+        BGMController.GetComponent<BGMController>().StartLoop();
+        foreach (AudioSource source in AmbientController.GetComponents<AudioSource>())
+        {
+            StartCoroutine(FadeOut(source, 0.75f));
+        }
+    }
+
+    // Fade out the ambient noise.
+    // Credit to Boris1998 at https://forum.unity.com/threads/fade-out-audio-source.335031/
+    public static IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+
+            yield return null;
+        }
+
+        audioSource.Stop();
+        audioSource.volume = startVolume;
     }
 
     // Draw the OOB box in the editor
