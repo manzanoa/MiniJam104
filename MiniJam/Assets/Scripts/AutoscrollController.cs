8f8f10cec741dbf20215c1bac7eace7ec2998664
Minescratcher452 <minescratcher452@gmail.com>
2022-04-17 21:51:39 -0400

    BGM and scrolling start on first click

--- MiniJam/Assets/Scripts/AutoscrollController.cs
+++ MiniJam/Assets/Scripts/AutoscrollController.cs
@@ -8,6 +8,8 @@ public class AutoscrollController : MonoBehaviour
     public float CAMERA_SPEED = 1f; // Camera speed in UU per second
     public BoxCollider2D left, right, top, deathPlaneColl2D;
     public GameObject frog;
+    public BGMController BGMController;
+    private int moveCamera = 0;
 
     // Used to draw collision boxes in editor
     Color collColor = Color.blue;
@@ -16,13 +18,25 @@ public class AutoscrollController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(moveCamera == 0 && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Pressed");
+            ActivateCamera(); 
+        }
+
         // Move the camera up slowly
-        transform.position = new Vector3(transform.position.x, transform.position.y + (CAMERA_SPEED * Time.deltaTime), transform.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y + (CAMERA_SPEED * Time.deltaTime * moveCamera), transform.position.z);
 
         if(frog.GetComponent<FrogMovement>().gameOver)
         {
             CAMERA_SPEED = 0;
         }
+    }
+
+    public void ActivateCamera()
+    {
+        moveCamera = 1;
+        BGMController.StartLoop();
     }
 
     // Draw the OOB box in the editor
