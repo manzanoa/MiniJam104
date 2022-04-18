4b382cb9a0126fa64fed6b41404ed8f5cdcceee8
Adrian <36515623+manzanoa@users.noreply.github.com>
2022-04-18 02:50:37 -0400

    Possible fix

--- MiniJam/Assets/TimeReducer.cs
+++ MiniJam/Assets/TimeReducer.cs
@@ -7,7 +7,7 @@ public class TimeReducer : MonoBehaviour
     public TrashGenerator trashGenerator;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.GetComponent<GameObject>().tag == "Player")
+        if(collision.tag == "Player")
         {
             trashGenerator.time -= 5;
         }
