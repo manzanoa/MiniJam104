a5c4772bc207f5126466f227d6988b16d7dcac7a
Minescratcher452 <minescratcher452@gmail.com>
2022-04-19 14:09:00 -0400

    foo

--- MiniJam/Assets/Scripts/TimeReducer.cs
+++ MiniJam/Assets/Scripts/TimeReducer.cs
@@ -7,9 +7,16 @@ public class TimeReducer : MonoBehaviour
     public TrashGenerator trashGenerator;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+<<<<<<< Updated upstream
         if(collision.tag == "Player")
         {
             trashGenerator.time -= 5;
+=======
+        if (collision.tag == "Player")
+        {
+            trashGenerator.time -= 5;
+            Destroy(this);
+>>>>>>> Stashed changes
         }
     }
 }
--- MiniJam/Assets/Scripts/TrashGenerator.cs
+++ MiniJam/Assets/Scripts/TrashGenerator.cs
@@ -11,21 +11,40 @@ public class TrashGenerator : MonoBehaviour
 
     public void StartTrash()
     {
+<<<<<<< Updated upstream
+=======
+        int x = Random.Range(0, 4);
+
+        GameObject trash = Instantiate(trashes[x], points[1].transform.position, Quaternion.identity);
+
+>>>>>>> Stashed changes
         StartCoroutine(trashSpawn());
     }
 
     IEnumerator trashSpawn()
     {
+<<<<<<< Updated upstream
         while(!frog.GetComponent<FrogMovement>().gameOver)
         {
+=======
+        while (!frog.GetComponent<FrogMovement>().gameOver)
+        {
+            yield return new WaitForSeconds(time);
+
+>>>>>>> Stashed changes
             int num = Random.Range(0, 5);
             int x = Random.Range(0, 4);
 
             GameObject trash = Instantiate(trashes[x], points[num].transform.position, Quaternion.identity);
+<<<<<<< Updated upstream
 
             yield return new WaitForSeconds(time);
         }
     }
 
 
+=======
+        }
+    }
+>>>>>>> Stashed changes
 }
